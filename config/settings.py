dd8f4a17846991148b0d858d5ea68c9e883c4a04
Adib Zaman <az@localhost.localdomain>
2026-08-04 12:18:56 -0400

    Hiding secrets in .env

--- config/settings.py
+++ config/settings.py
@@ -1,20 +1,16 @@
-import os
 from pydantic_settings import BaseSettings
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class AppSettings(BaseSettings):
-    APP_ENV: str = os.getenv("APP_ENV", "development")
-    API_PORT: int = int(os.getenv("API_PORT", 8000))
-    REDIS_HOST: str = os.getenv("REDIS_HOST", "queue_broker")
-    REDIS_PORT: int = int(os.getenv("REDIS_PORT", 6379))
-    EXTERNAL_COMPLIANCE_API_URL: str = os.getenv(
-        "EXTERNAL_COMPLIANCE_API_URL", "https://api.mockcompliance.org/v1/validate"
-    )
-    EXTERNAL_API_TIMEOUT_SEC: float = float(os.getenv("EXTERNAL_API_TIMEOUT_SEC", 5.0))
+    APP_ENV: str = "development"
+    API_PORT: int = 8000
+    REDIS_HOST: str = "queue_broker"
+    REDIS_PORT: int = 6379
+    EXTERNAL_COMPLIANCE_API_URL: str = "https://api.mockcompliance.org/v1/validate"
+    EXTERNAL_API_TIMEOUT_SEC: float = 5.0
 
-    LLM_ENDPOINT_URL: str = os.getenv("LLM_ENDPOINT_URL", "https://studio.bearcatgpt.uc.edu/api/v1/")
-    LLM_ACCESS_TOKEN: str = os.getenv("LLM_ACCESS_TOKEN", "sk-ce534a4abbe64dd3a51bb50d9985d6f1")
+    LLM_ENDPOINT_URL: str = "https://studio.bearcatgpt.uc.edu/api/v1/"
+    LLM_ACCESS_TOKEN: str
+
+    model_config = {"env_file": ".env"}
 
 settings = AppSettings()
